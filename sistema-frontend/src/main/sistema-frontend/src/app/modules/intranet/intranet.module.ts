cbd008ad02f08a6cb047746578083a29bc2d3cb0
Arturo <luisarturorodriguezcarrillo@gmail.com>
2022-07-20 19:25:18 -0500

    [main]-[Acmbieos]

--- sistema-frontend/src/main/sistema-frontend/src/app/modules/intranet/intranet.module.ts
+++ sistema-frontend/src/main/sistema-frontend/src/app/modules/intranet/intranet.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { IntranetRoutingModule } from './intranet-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { MaterialModule } from '../material.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    IntranetRoutingModule
+    IntranetRoutingModule,
+    MaterialModule
   ]
 })
 export class IntranetModule { }
